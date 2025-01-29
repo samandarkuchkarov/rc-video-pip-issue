cb5a5644a169d18c8d6c2982945b7d55798c57e1
samandar <samandar8939522@gmail.com>
2025-01-29 10:58:46 +0500

    add button to call enterPictureInPicture

--- App.tsx
+++ App.tsx
@@ -7,34 +7,44 @@
 
 import React, {useEffect} from 'react';
 import type {PropsWithChildren} from 'react';
+import {Text, TouchableOpacity, View} from 'react-native';
 
 import Video, {VideoRef} from 'react-native-video';
 
 function App(): React.JSX.Element {
   const video = React.useRef<VideoRef | null>(null);
 
-  useEffect(() => {
-    setTimeout(() => {
-      console.log(video.current);
-      video.current?.enterPictureInPicture();
-    }, 5000);
-  }, []);
   return (
-    <Video
-      ref={video}
-      style={{flex: 1}}
-      enterPictureInPictureOnLeave
-      source={{
-        uri: 'https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8',
-      }}
-      onLoad={e => {
-        video.current?.resume();
-      }}
-      resizeMode="contain"
-      onError={e => {
-        console.warn(e);
-      }}
-    />
+    <View style={{flex: 1}}>
+      <Video
+        ref={video}
+        style={{width: '100%', height: 300}}
+        enterPictureInPictureOnLeave
+        source={{
+          uri: 'https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8',
+        }}
+        resizeMode="contain"
+        onError={e => {
+          console.warn(e);
+        }}
+      />
+      <TouchableOpacity
+        style={{
+          width: '100%',
+          height: 50,
+          backgroundColor: 'red',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        onPress={() => {
+          video.current?.enterPictureInPicture();
+          console.log('enterPictureInPicture called');
+        }}>
+        <Text style={{color: 'white', textAlign: 'center', fontSize: 22}}>
+          Enter Picture in Picture
+        </Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
